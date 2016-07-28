df6600a0d63f61fa5e70883a7a67415cea920be0
James Rouzier <jrouzier@inverse.ca>
2016-07-28 11:35:38 -0400

    Control directory

--- lib/pf/file_paths.pm
+++ lib/pf/file_paths.pm
@@ -84,6 +84,7 @@ our (
     $captiveportal_templates_path,
     $captiveportal_profile_templates_path,
     $captiveportal_default_profile_templates_path,
+    $control_dir
 );
 
 BEGIN {
@@ -142,6 +143,7 @@ BEGIN {
         $captiveportal_templates_path
         $captiveportal_profile_templates_path
         $captiveportal_default_profile_templates_path
+        $control_dir
     );
 }
 
@@ -158,6 +160,7 @@ $log_conf_dir  = catdir( $conf_dir,"log.conf.d" );
 
 $generated_conf_dir   = catdir( $var_dir,"conf");
 $tt_compile_cache_dir = catdir( $var_dir,"tt_compile_cache");
+$control_dir  = catdir( $var_dir,"control");
 $pfconfig_cache_dir = catdir( $var_dir,"cache/pfconfig");
 $domains_chroot_dir = catdir( "/chroots");
 

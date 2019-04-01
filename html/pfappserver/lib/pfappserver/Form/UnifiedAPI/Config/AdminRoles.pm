34fc6822c3fe9ebbd3a556ce8da6f39cf2b7ef53
James Rouzier <jrouzier@inverse.ca>
2019-04-01 18:17:48 -0400

    Make allowed_access_durations an array

--- html/pfappserver/lib/pfappserver/Form/UnifiedAPI/Config/AdminRoles.pm
+++ html/pfappserver/lib/pfappserver/Form/UnifiedAPI/Config/AdminRoles.pm
@@ -1,55 +1,35 @@
-#!/usr/bin/perl
+package pfappserver::Form::UnifiedAPI::Config::AdminRoles;
 
 =head1 NAME
 
-AdminRoles
-
-=cut
+pfappserver::Form::UnifiedAPI::Config::AdminRoles -
 
 =head1 DESCRIPTION
 
-unit test for AdminRoles
+pfappserver::Form::UnifiedAPI::Config::AdminRoles
 
 =cut
 
 use strict;
 use warnings;
-#
-use lib qw(
-    /usr/local/pf/lib
+
+use HTML::FormHandler::Moose;
+extends 'pfappserver::Form::Config::AdminRoles';
+
+has_field 'allowed_access_durations' => (
+   type => 'CSV',
+   multiple => 1,
+   id => 'allowed_access_durations',
+   label => 'Allowed user access durations',
+   tags => { after_element => \&help,
+             help => 'A comma seperated list of access durations available to the admin user. If none are provided then the default access durations are used'},
+   input_param => 'allowed_access_durations',
+   accessor => 'allowed_access_durations',
 );
 
-BEGIN {
-    #include test libs
-    use lib qw(/usr/local/pf/t);
-    #Module for overriding configuration paths
-    use setup_test_config;
-}
-
-use Test::More tests => 11;
-use Test::Mojo;
-
-#This test will running last
-use Test::NoWarnings;
-my $t = Test::Mojo->new('pf::UnifiedApi');
-
-my $collection_base_url = '/api/v1/config/admin_roles';
-
-my $base_url = '/api/v1/config/admin_role';
-
-$t->get_ok($collection_base_url)
-  ->status_is(200);
-
-$t->post_ok($collection_base_url => json => {})
-  ->status_is(422);
-
-$t->post_ok($collection_base_url, {'Content-Type' => 'application/json'} => '{')
-  ->status_is(400);
-
-$t->get_ok("$base_url/User Manager")
-  ->status_is(200)
-  ->json_is('/item/id', 'User Manager')
-  ->json_has('/item/allowed_access_durations', 'allowed_access_durations');
+has_field 'allowed_access_durations.contains' => (
+    type => 'Duration',
+);
 
 =head1 AUTHOR
 
@@ -79,3 +59,4 @@ USA.
 =cut
 
 1;
+
--- lib/pf/UnifiedApi/Controller/Config/AdminRoles.pm
+++ lib/pf/UnifiedApi/Controller/Config/AdminRoles.pm
@@ -16,11 +16,11 @@ use strict;
 use warnings;
 use Mojo::Base qw(pf::UnifiedApi::Controller::Config);
 use pf::ConfigStore::AdminRoles;
-use pfappserver::Form::Config::AdminRoles;
+use pfappserver::Form::UnifiedAPI::Config::AdminRoles;
 use pf::config qw(%ConfigAdminRoles);
 
 has 'config_store_class' => 'pf::ConfigStore::AdminRoles';
-has 'form_class' => 'pfappserver::Form::Config::AdminRoles';
+has 'form_class' => 'pfappserver::Form::UnifiedAPI::Config::AdminRoles';
 has 'primary_key' => 'admin_role_id';
 
 sub items {
